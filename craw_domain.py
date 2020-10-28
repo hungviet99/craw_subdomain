2b9888025c849ef1f1ef7e16934a7faaf11c7be3
hungviet99 <hungvietnguyen6241@gmail.com>
2020-10-28 11:23:25 +0700

    update

--- craw_domain.py
+++ craw_domain.py
@@ -6,36 +6,32 @@ import config
 import sys
 
 domain = input("Nhap vao domain: ")
-
-# Connect API nmap
 nmap = nmap3.Nmap()
-sub_nmap = nmap.nmap_dns_brute_script(domain)
-# Connect API SecurityTrails
-st = SecurityTrails(config.api_st)
-sub_st = st.domain_subdomains(domain)
-sudomain_st = sub_st['subdomains']
-# Connect API VirusTotal
 vt = VirusTotalPublicApi(config.api_vt)
-sub_vt = vt.get_domain_report(domaini)
+
+sub_nmap = nmap.nmap_dns_brute_script(domain)
+
+#st = SecurityTrails(config.api_st)
+#sub_st = st.domain_subdomains(domain)
+#sudomain_st = sub_st['subdomains']
+sub_vt = vt.get_domain_report(domain)
+
 subdomain_vt = sub_vt['results']['subdomains']
 
+
 def nmap_scan(sub_nmap):
-    """Xử lý dữ liệu của nmap
-    """
     list_nmap = []
     for dist in sub_nmap:
         apsub = dist['hostname']
         list_nmap.append(apsub)
     return list_nmap
 
-def st_and_vt_scan(sudomain_st, list_nmap, subdomain_vt):
-    """Xử lý dữ liệu của securityTrails và Virustotal
-    Xử lý các kết quả trùng nhau
-    """
+def st_and_vt_scan(list_nmap, subdomain_vt):
+
     list_st = []
-    for element in sudomain_st:
-        subdm = element+'.'+domain
-        list_st.append(subdm)
+#    for element in sudomain_st:
+#        subdm = element+'.'+domain
+#        list_st.append(subdm)
 
     for element in list_nmap:
         if element not in list_st:
@@ -44,12 +40,12 @@ def st_and_vt_scan(sudomain_st, list_nmap, subdomain_vt):
     for element in subdomain_vt:
         if element not in list_st:
             list_st.append(element)
+
     return list_st
+
 
 # Kiam tra va loai bo cac domain cũ
 def check_subdomain(list_st):
-    """Kiểm tra các bản ghi của domain, loại bỏ domain cũ
-    """
     sub_all = {}
     for x in list_st:
         try:
@@ -60,9 +56,7 @@ def check_subdomain(list_st):
     return sub_all
 
 def main():
-    """Lấy dữ liệu về các domain, ghi vào sub_all
-    """
     list_nmap = nmap_scan(sub_nmap)
-    list_st = st_and_vt_scan(sudomain_st, list_nmap, subdomain_vt)
+    list_st = st_and_vt_scan(list_nmap, subdomain_vt)
     sub_all = check_subdomain(list_st)
     return sub_all
--- database.py
+++ database.py
@@ -3,21 +3,20 @@ import sys
 import json
 import craw_domain
 
-# Thực hiện connect tới database
+
+
 try:
     conn = pymysql.connect(host='localhost',
-                            user='config.user',
-                            password='config.password',  
+                            user='root',  
                             database="craw_domain", 
                             port=3306)
 except Exception as ex:
     print("Error connecting: " + str(ex))
     sys.exit(1)
+
 cur = conn.cursor()
 
 def select_db(cur, domain):
-    """Lấy ra dữ liệu của subdomain từ Mysql
-    """
     try:
         sub_in_mysql= {}
         cur.execute("SELECT subdomain from subdomain where domain ='{}'".format(domain))
@@ -32,17 +31,11 @@ def select_db(cur, domain):
     return sub_in_mysql
 
 def insert_db(cur, mydomain, sub_all):
-    """Ghi dữ liệu vào Mysql
-    """
     cur.execute("DELETE  FROM subdomain WHERE domain='{}'".format(mydomain))
     cur.execute("INSERT INTO subdomain (domain, subdomain) values ('"+mydomain+"','"+sub_all+"')")
     conn.commit()
 
-def main(mydomain, cur):
-    """Xử lý dữ liệu
-    Nếu không có dữ liệu trong database thì ghi luôn vào database
-    Nếu có dữ liệu cũ thì cập nhật dữ liệu rồi ghi vào database
-    """
+def main():
     mydomain = craw_domain.domain
     sub_all = craw_domain.main()
     sub_in_mysql = select_db(cur, mydomain)
