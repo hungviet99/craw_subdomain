28ee6f79efe0b74a517e226ee60f8babe6be646e
hungviet99 <hungvietnguyen6241@gmail.com>
2020-10-30 19:46:29 +0700

    update

---
+++ tele_subdomain/Telebot.py
@@ -0,0 +1,54 @@
+import telebot
+import re
+import config
+import elasticdb
+import craw_domain
+
+bot = telebot.TeleBot(config.TOKEN_TELE)
+
+@bot.message_handler(commands=["start"])
+def send_devices(message):
+    """
+        Tạo lệnh start để hướng dẫn sử dụng 
+    """
+    bot.reply_to(message, "Nhập vào /domain <tên domain> " +
+                        "để xem các subdomain. VD: /domain hungnv.com")
+
+def main():
+    @bot.message_handler(commands=["domain"])
+    def send_subdomain(message):
+        """Tạo lệnh để truyền vào domain từ telegram
+        """
+        domain = message.text[8:]
+        sub_all = craw_domain.main(domain)
+        a = elasticdb.main(domain, sub_all)
+        res = elasticdb.search_by_index_and_id(domain)
+        res2 = res['_source']
+        res2.update(sub_all)
+        sub_all_1 = []
+        for key, value in res2.items():
+            sub_all_1.append(key)
+        sub_all_cv = str(sub_all_1)
+        if len(sub_all_cv) > 4096:
+            for x in range(0, len(sub_all_cv), 4096):
+                try:
+                    bot.send_message(config.CHAT_ID, "Tìm thấy: " +
+                                     str(len(sub_all_1)) +
+                                     " subdomain" + '\n\n' +
+                                     sub_all_cv[x:x+4096],
+                                     parse_mode='Markdown')
+                except:
+                    bot.send_message(config.CHAT_ID, "Tìm thấy: " +
+                                     str(len(sub_all_1)) + 
+                                     " subdomain" + '\n\n' +
+                                     sub_all_cv[x:x+4096])
+        else:
+            try:
+                bot.send_message(config.CHAT_ID, "Tìm thấy: " +
+                                 str(len(sub_all_1)) + " subdomain" + '\n\n' +
+                                 sub_all_cv, parse_mode='Markdown')
+            except:
+                bot.send_message(config.CHAT_ID, "Tìm thấy :" +
+                                 str(len(sub_all_1)) + " subdomain" + '\n\n' +
+                                 sub_all_cv)
+    bot.polling()
