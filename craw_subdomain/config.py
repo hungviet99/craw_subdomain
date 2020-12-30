212ae6b8c57b0c7c93b7a1d6cdb561ef8ff10555
hungviet99 <hungvietnguyen6241@gmail.com>
2020-12-30 15:47:11 +0700

    update

--- craw_subdomain/config.py
+++
@@ -1,5 +0,0 @@
-#api virus total
-api_vt =
-
-# security train
-api_st = "17cd6d28652ea7dd99"
--- craw_subdomain/main.py
+++
@@ -1,5 +0,0 @@
-import database
-
-if __name__ == "__main__":
-    database.main()
-    print(database.main())
